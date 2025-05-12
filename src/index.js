ffba5694d98fe26ca8bdac75f7d10ac0b2b12e54
elifred2022 <elifed21@hotmail.com>
2025-05-12 20:28:26 -0300

    para que funcione en gitpage y el local se tuvo que hacer codigos de condicional en index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/ordenespvc" : "/";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
 );
